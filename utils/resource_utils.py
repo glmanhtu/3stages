20d45ae04b96c06b5a420a3681b25ff57226d2c3
Manh Tu VU <glmanhtu@gmail.com>
2020-09-17 16:20:27 +0200

    Updat

--- utils/resource_utils.py
+++ utils/resource_utils.py
@@ -27,13 +27,40 @@ def get_checkpoint_file_path(name):
         return checkpoint_file
     subject, mode = tuple(name.replace('.ckpt', '').split('_'))
     file_id = pretrained[subject][mode]
-    file_url = 'https://drive.google.com/uc?export=download&id=' + file_id
-    s = requests.Session()
-    s.mount('https://', HTTPAdapter(max_retries=10))
-    r = s.get(file_url, allow_redirects=True)
-    with open(checkpoint_file, 'wb') as cf:
-        cf.write(r.content)
+    download_file_from_google_drive(file_id, checkpoint_file)
     return checkpoint_file
+
+
+def download_file_from_google_drive(file_id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params={'id': file_id}, stream=True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = {'id': file_id, 'confirm': token}
+        response = session.get(URL, params=params, stream=True)
+
+    save_response_content(response, destination)
+
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
 
 
 def get_cache_path(name):
